8332d7cb82ada565b1244bb6cfbd80f4bd7c704e
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2024-01-07 01:01:11 +0530

    initial commit

---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";  // treat all js code as newer version
+// alert(3+3)   // we are using nodejs, not browser
+// number => 2 to power 53
+// bigint 
+// boolean => true/false
+// string => ""
+// null=> standalone value
+// undefined => 
+// symbol=> unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
+
---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId = 1234321
+let accountEmail ="riya@gmail.com"
+var accountPassword ="xyz"
+accountCity ="Birdwan"
+// accountId =2 //not allowed
+accountEmail="ghonu@gmail.com"
+accountPassword="9876"
+accountCity ="Durgapur"
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+
+
+
