0ea8c95738d4b69642c82b44b2cc8d5ee8cf8773
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2024-01-12 16:52:20 +0530

    initial commit

--- functions.js
+++ functions.js
@@ -25,10 +25,37 @@ const result =  addTwoNumbers(3,5)
 // console.log(result);
 function loginUserMessage(username){
     if(username=== undefined){
-        console.log("plz enter a username");
+        // console.log("plz enter a username");
         return
     }
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("hitesh"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1,val2,...num1){                   //rest operator
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500,2000));
+const user ={
+    username: "Hitesh",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[0]
+
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue[200,400,500,1000]);
+
+
