fb19a7e0798d26571b28409195b8510a78972afa
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2024-01-14 16:13:26 +0530

    initial commit

--- controlFlow.js
+++ controlFlow.js
@@ -54,5 +54,66 @@ const balance = 10000
 // }
 
 
+const month = 3
+switch(month) {
+    case 1:
+        // console.log("January");
+        break;
+    case 2:
+        // console.log("February");
+        break;
+    case 3:
+        // console.log("March");
+        break;
+    case 4:
+        // console.log("April");
+        break;
+    default:
+        // console.log("default case match");
+        break;
+}
+
+
+
+
+// truthy or false     // truthy means assumtion
+
+const userEmail = "h@hitesh.ai"
+if(userEmail){
+    // console.log("Got user email");
+}
+else{
+    // console.log("Don't have user email");
+}
+
+//falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// truthy values
+// "0", "false", " ", [],{}, function(){}
+// if(userEmail.length===0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj ={}
+if(Object.keys(emptyObj).length ===0){
+    // console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator(??): null undefined     
+let val1;
+// val1= 5 ?? 10             [nullish coalescing and ternary operator those are different]
+// val1= null ?? 10
+// val1= undefined ?? 15
+// val1= null ?? 10 ?? 20
+// console.log(val1);
+
+// Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80");;
+
+
 
 
