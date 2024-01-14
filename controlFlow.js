fd6c469498d1e1ab054f9e21c588601503572597
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2024-01-14 15:34:56 +0530

    initial commit

---
+++ controlFlow.js
@@ -0,0 +1,58 @@
+// if
+// if(2===2){
+//     console.log("executed");
+// }
+// const isUserLoggedIn = true
+// const temperature = 41
+// if(temperature<9){
+//     console.log("less than 9");
+// }
+// console.log("temparature is greater than 9");
+
+// const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log((`user power:${power}`));
+// }
+// console.log((`user power:${power}`));
+
+const balance = 10000
+// if(balance>5000) console.log("test"),console.log("test2");        // not readable
+// if(balance>5000){
+//     console.log("less than 5000");
+// }
+// else if(balance<750){
+//     console.log("less than 7500");
+// }
+// else if(balance<9000){
+//     console.log("less than 9000");
+// }
+// else{
+//     console.log("less than 12000");
+// }
+
+
+
+// const isUserLoggedIn = true
+// const debitCard = true
+// const loggedInGoogle = false
+// const loggedInFromEmail = true 
+// if(isUserLoggedIn && debitCard && 2==2){
+//     console.log("Allow to buy course");
+// }
+// if(loggedInFromEmail || loggedInGoogle){
+//     console.log("user logged in");
+// }
+
+
+// switch syntax
+// switch(key){
+//     case value:
+//         break;
+//         default:
+//             break;
+// }
+
+
+
+
--- executionContext.js
+++ executionContext.js
@@ -3,3 +3,17 @@
 // Eval execution context
 // 1st phase- memory/ creation phase  undefined
 // 2nd phase - execution phase
+
+function one(){
+    console.log(one);
+
+}
+function two(){
+    console.log(two);
+}
+function three(){
+    console.log(three);
+}
+one()
+two()
+three()
