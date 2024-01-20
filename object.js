c558eca68c71f4cd90f2e0c257764d7b9e582896
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2024-01-20 21:14:49 +0530

    initial commit

--- object.js
+++ object.js
@@ -2,33 +2,73 @@
 
 // object literals
 
-const mySym = Symbol("key1")
-const jsUser = {
-    name: "Hitesh",
-    [mySym]: "mykey1",
-    age: 18,
-    location: "Burswan",
-    email: "hitesh@google.com",
-    isLoggedIn: false,
-    lastLoggedinDays: ["Monday", "Saturday"]
-}
+// const mySym = Symbol("key1")
+// const jsUser = {
+//     name: "Hitesh",
+//     [mySym]: "mykey1",
+//     age: 18,
+//     location: "Burswan",
+//     email: "hitesh@google.com",
+//     isLoggedIn: false,
+//     lastLoggedinDays: ["Monday", "Saturday"]
+// }
 // console.log(jsUser.email);
 // console.log(jsUser["email"]);
 // console.log( typeof jsUser [mySym]);
-jsUser.email="hitesh.chatgpt.com"
+// jsUser.email="hitesh.chatgpt.com"
 //  console.log(jsUser.email);
 // Object.freeze(jsUser)
 
 
 
-jsUser.greeting = function(){
-    console.log(`hello jsUser`);
-}
-jsUser.greetingTwo = function(){
-    console.log(`hello jsUser, ${this.name}`);
-}
-console.log(jsUser.greeting());
-console.log(jsUser.greetingTwo());
+// jsUser.greeting = function(){
+    // console.log(`hello jsUser`);
+// }
+// jsUser.greetingTwo = function(){
+    // console.log(`hello jsUser, ${this.name}`);
+// }
+// console.log(jsUser.greeting());
+// console.log(jsUser.greetingTwo());
+
+
+
+// const tinderUser = new Object()
+
+// const tinderUser ={}
+//     tinderUser.id="12345",
+//     tinderUser.name= "riya"
+//     tinderUser.isLoggedIn= false
+// console.log(tinderUser);
+
+
+
+
+//     const   regularUser ={
+//         email: "riya@yahoo.com",
+//         fullname:{
+//             userfullname:{
+//                 firstname:"riya",
+//                 lastname:"hati"
+//             }
+//         }
+//     }
+    // console.log(regularUser.fullname.firstname);
+
+    // const obj1 = {1: "a", 2: "b"}
+    // const obj2 = {3: "a", 4: "b"}
+    // const obj3 ={obj1, obj2}
+    // const obj3 = Object.assign({}, obj1, obj2)
+    // console.log(obj3);
+    // const obj3={...obj1,...obj2}
+    // console.log(obj3);
+
+
+
+
+
+
+    
+
 
 
 
